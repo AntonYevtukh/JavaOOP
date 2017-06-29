e26a200c76f586228f96012496a0a2c71951854d
Anton <anton.yevtukh@gmail.com>
2017-06-30 02:52:01 +0300

    All tasks for lection 1 are done.

---
+++ src/additional/lection1/task02/Triangle.java
@@ -0,0 +1,39 @@
+package additional.lection1.task02;
+
+public class Triangle {
+
+    private final int a;
+    private final int b;
+    private final int c;
+
+    Triangle(int a, int b, int c) {
+        if ((a < b + c) && (b < a + c) && (c < a + b)) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        else {
+            this.a = 1;
+            this.b = 1;
+            this.c = 1;
+        }
+    }
+
+    Triangle(Triangle triangle) {
+        this.a = triangle.a;
+        this.b = triangle.b;
+        this.c = triangle.c;
+    }
+
+    private int perimeter() {
+        return a + b + c;
+    }
+
+    public double area() {
+        return Math.sqrt(perimeter() / 2 * (perimeter() / 2  - a) * (perimeter() / 2 - b) * (perimeter() / 2 - c));
+    }
+
+    public String toString() {
+        return "Triangle: a = " + a + ", b = " + b + ", c = " + c + ", perimeter = " + perimeter() + ", area = " + area() + ".";
+    }
+}
---
+++ src/additional/lection1/task02/TriangleRunner.java
@@ -0,0 +1,12 @@
+package additional.lection1.task02;
+
+public class TriangleRunner
+{
+    public static void main(String[] args) {
+
+        Triangle egyptTriangle = new Triangle(3, 4, 5);
+        Triangle chinaTriangle = new Triangle(5, 12, 13);
+        System.out.println(egyptTriangle);
+        System.out.println(chinaTriangle);
+    }
+}
