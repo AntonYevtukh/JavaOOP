120e9957fdf3dd8489a203a33747cd6b17bdc83c
Anton <anton.yevtukh@gmail.com>
2017-07-03 18:35:20 +0300

    All tasks for lections 3-4 done

--- src/homework/lection04/task02/FsItem.java
+++ src/homework/lection04/task02/FsItem.java
@@ -18,6 +18,12 @@ public abstract class FsItem {
         this.name = name;
     }
 
+    /**
+     * Проверяет, не "содержится" ли директория target в вызывающем объекте или его подпапках. Если вызывающий объект - это
+     * файл, просто возвращает false. Если папка - проверяет в ней и рекурсивно в ее содержимом. Возвращает true, если
+     * папка target найдена. Мне кажется, что не очень хорошо, что данный метод (как и getContent)
+     * есть и у файла, но иначе пришлось бы использовать instanceof.
+     */
     protected abstract boolean checkEntry(Directory target); //объявлене костыля раз
     protected abstract List<FsItem> getContent(); //объявление костыля два
 
